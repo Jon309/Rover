338318ce20254b59d2e33f16a5eb6841f1e84265
Jon Henshaw <jhensh309@gmail.com>
2019-10-23 23:50:46 -0400

    General Layout Completed

---
+++ Rover.Repository/UserRepository.cs
@@ -0,0 +1,37 @@
+using Rover.Entities;
+using Rover.Interfaces;
+using System;
+using System.ComponentModel.Design;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Threading.Tasks;
+using Dapper;
+using Rover.DataAccess;
+
+namespace Rover.Repository
+{
+    public sealed class UserRepository : IUserRepository
+    {
+        private readonly IDbConnection _connection = new ConnectionFactory().ConnectRover();
+
+        
+        
+        // GOTTA FIGURE OUT HOW TO CREATE USER OBJECT FROM THIS
+        public async Task<User> GetUserById(int userId)
+            => await _connection.QueryFirstOrDefaultAsync<User>(@"
+SELECT userId
+    first_name
+    last_name
+    email
+    description
+FROM Apollo.Rover.Users
+WHERE userId = @userId;", new {userId});
+        
+        public async Task DeleteUser(int userId)
+            => await _connection.ExecuteAsync(@"
+DELETE FROM Apollo.Rover.Users 
+WHERE userId = @userId", new {userId});
+
+    }
+}
---
+++ Rover.WebApi/Startup.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Autofac;
+using CacheRepository.Implementation;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Hosting.Internal;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Caching.Memory;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using Rover.Repository;
+using Rover.ServiceInterfaces;
+using Rover.Services;
+using Thinktecture.IdentityServer.Repositories;
+using IUserRepository = Rover.Interfaces.IUserRepository;
+
+namespace Rover
+{
+    public class Startup
+    {
+        private const string ApiVersion = "V1";
+        private const string ServiceName = "Rover";
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+            services.AddSingleton<IUserRepository, UserRepository>();
+            services.AddSingleton<IUserService, UserService>();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseMvc();
+        }
+    }
+}
